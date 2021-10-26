bca831f5fbcbc257de23d41a61478730b39e95c4
Jatin Sai <51705706+jtn07@users.noreply.github.com>
2021-10-27 03:31:52 +0530

    Delete EmailAddressNotFoundException.java

--- src/main/java/com/notyetdecided/mailsender_microservice/Exceptions/EmailAddressNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.notyetdecided.mailsender_microservice.Exceptions;
-
-public class EmailAddressNotFoundException {
-    public String EmailAddressNotFoundException(){
-        return "Email Not Found";
-    }
-}
